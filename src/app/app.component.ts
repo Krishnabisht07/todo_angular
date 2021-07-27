0150474a65e86e5ff17c7a8432c3cec9477ac69c
Krishnabisht07 <bishtkrishna7094@gmail.com>
2021-07-27 19:58:37 +0530

    first push for todo_angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+export interface DialogData {
+  title: string;
+  description: [];
+  action:string;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +13,118 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Todo';
+ 
+  titleG = '';
+  descriptionG = '';
+  listTodos = [{
+    id: '1',
+    title: 'Breakfast',
+    description: 'I will have breakfast today around 8 am',
+    status: 'active'
+  },
+  {
+    id: '2',
+    title: 'Lunch',
+    description: 'I will have breakfast today around 1 pm',
+    status: 'active'
+  },
+  {
+    id: '3',
+    title: 'Dinner',
+    description: 'I will have breakfast today around 9 pm',
+    status: 'active'
+  }
+
+  ];
+  // completedTodos:any;
+  // activeTodos: any;  
+  // displayComplete:boolean = false;
+  // displayActive:boolean = false;
+  constructor(public dialog: MatDialog) {
+
+  }
+  deleteTodo(num: string,index?:number) {
+    let i = this.listTodos.findIndex(data=> (data.id==num));
+    this.listTodos.splice(Number(i) ,1);
+    // if(index==1)
+    // this.changeTab(index)
+  }
+  changeTab(index:number){
+    // if(index==1)
+    // {
+    //  if(this.listTodos.filter(data => data.status=="completed"))
+    //  {
+    //    this.displayComplete = true;
+    //    this.displayActive = false;
+    //  }
+    // }
+    // else if(this.listTodos.filter(data => data.status=="active"))
+    // {
+    //   this.displayActive = true;
+    //   this.displayComplete = false;
+    // }
+    // else{
+    //   this.displayActive = false;
+    //   this.displayComplete = false;
+    // }
+  }
+  openDialog(action:string,id?:string) {
+    if(action=='display')
+    {
+      let data = this.listTodos.find(data => data.id==id);
+      if(data){
+      this.titleG = data.title;
+      this.descriptionG = data.description
+      }
+    }
+    else{
+      this.titleG='';
+      this.descriptionG='';
+    }
+
+    const dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
+      width: '250px',
+      data: { title: this.titleG, description: this.descriptionG, action:action}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        let id = this.listTodos.length+1;
+        result.id = id;
+        result.status="active";
+        this.listTodos.push(result);
+      }
+    });
+  }
+  changeStatus(checked: boolean, id: string) {
+
+   let todo = this.listTodos.find(data => {
+      return data.id==id;
+    });
+    if(todo && checked)
+    {
+    todo.status = "completed";
+    }
+    else if(todo){
+    todo.status = "active";
+    }
+  
+   }
+}
+@Component({
+  selector: 'example-dialog',
+  templateUrl: 'dialogPop.html',
+})
+export class DialogOverviewExampleDialog {
+
+  constructor(
+    public dialogRef: MatDialogRef<DialogOverviewExampleDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData, private dialogRef1: MatDialogRef<DialogOverviewExampleDialog>) {
+    dialogRef1.disableClose = true;
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,43 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent, DialogOverviewExampleDialog } from './app.component';
+
+import {MatCardModule} from '@angular/material/card';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import { MatFormFieldModule, MatLabel } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DialogOverviewExampleDialog
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatCardModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatButtonModule,
+    MatTabsModule,
+    MatIconModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatDialogModule
+  ],
+  entryComponents: [
+    DialogOverviewExampleDialog
   ],
   providers: [],
   bootstrap: [AppComponent]
